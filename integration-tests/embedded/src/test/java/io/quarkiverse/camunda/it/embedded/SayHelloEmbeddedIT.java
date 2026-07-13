06605e77b8147ce057976f33d9acc78e931efc81
Nico Hinrichs <n_hinrichs@taures.de>
2026-07-13 14:26:12 +0200

    feat: remove embedded integration test module since embedded camunda engines does not exist anymore

--- integration-tests/embedded/src/test/java/io/quarkiverse/camunda/it/embedded/SayHelloEmbeddedIT.java
+++
@@ -1,7 +0,0 @@
-//package io.quarkiverse.camunda.it.embedded;
-//
-//import io.quarkus.test.junit.QuarkusIntegrationTest;
-//
-//@QuarkusIntegrationTest
-//public class SayHelloEmbeddedIT extends SayHelloEmbeddedTest {
-//}
